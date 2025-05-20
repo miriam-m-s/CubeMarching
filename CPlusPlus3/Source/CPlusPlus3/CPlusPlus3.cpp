ae9d6cde47757c142fb004d2b917b34996fb4505
MiriamMartín <mirima10@ucm.es>
2025-05-20 22:53:02 +0200

    deleted

--- CPlusPlus3/Source/CPlusPlus3/CPlusPlus3.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPlusPlus3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPlusPlus3, "CPlusPlus3" );
- 
